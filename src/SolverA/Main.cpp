d84f7e5d68982dd4176d4fdd4e9a64198072c6df
K naotti <keimahatokusyu@gmail.com>
2016-07-21 12:43:00 +0900

    drawに渡すパラメータを変更

--- src/SolverA/Main.cpp
+++ src/SolverA/Main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int) {
 
 	// while(裏画面を表画面に反映,メッセージ処理,画面クリア, ESCで終了)
 	while (ScreenFlip()==0 && ProcessMessage()==0 && ClearDrawScreen()==0 && CheckHitKey(KEY_INPUT_ESCAPE)==0) {
-		solver.draw(true);
+		solver.draw(false);
 	}
 	DxLib_End();
 	return 0;
